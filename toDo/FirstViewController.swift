bd1ea7a520637d38a2e2835c5d284d4def29c96d
David Keck <dstevenkeck@gmail.com>
2014-06-13 16:19:03 -0500

    finished toDo app

--- toDo/FirstViewController.swift
+++ toDo/FirstViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-                            
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tbltoDo: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,34 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        tbltoDo.reloadData()
+    }
+    
+    func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
+        
+        if(editingStyle == UITableViewCellEditingStyle.Delete) {
+            toDo.tasks.removeAtIndex(indexPath.row)
+        }
+        
+        tbltoDo.reloadData()
+    }
 
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return toDo.tasks.count
+        
+    }
+
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!{
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Default")
+        
+        cell.text = toDo.tasks[indexPath.row].name
+        cell.detailTextLabel.text = toDo.tasks[indexPath.row].desc
+        
+        return cell
+        
+    }
 
 }
 
--- toDo/SecondViewController.swift
+++ toDo/SecondViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var ttltoDo: UITextField!
+    @IBOutlet var dsctoDo: UITextField!
                             
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +22,30 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Add Task button
+    @IBAction func buttonAddTaskTouched(sender: UIButton) {
+        println("Add task was touched!")
+        toDo.addTask(ttltoDo.text, desc: dsctoDo.text)
+        ttltoDo.text = ""
+        dsctoDo.text = ""
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func buttonCancelTouched(sender: UIButton) {
+        println("Cancel was touched!")
+        self.view.endEditing(true)
+        self.tabBarController.selectedIndex = 0
+    }
+    
+    // UITextField delegeate
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }// called when 'return' key pressed. return NO to ignore.
 
 
 }
